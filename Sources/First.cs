c557fbf98af3266fb132a8b816687b9aa0014096
Srivatsa Haridas <Srivatsa.Haridas@Honeywell.com>
2017-03-10 11:57:25 +0530

    A new source has been added

---
+++ Sources/First.cs
@@ -0,0 +1,31 @@
+public class MainActivity extends Activity {
+    SampleAlarmReceiver alarm = new SampleAlarmReceiver();
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    // Menu options to set and cancel the alarm.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // When the user clicks START ALARM, set the alarm.
+            case R.id.start_action:
+                alarm.setAlarm(this);
+                return true;
+            // When the user clicks CANCEL ALARM, cancel the alarm. 
+            case R.id.cancel_action:
+                alarm.cancelAlarm(this);
+                return true;
+        }
+        return false;
+    }
+}
