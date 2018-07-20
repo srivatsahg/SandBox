a380d0812ab5fb740c2eba61a357fecfa873ef2a
Srivatsa Haridas <Srivatsa.Haridas@Honeywell.com>
2018-07-20 08:49:01 +0530

    Changed in master

--- Sources/First.cs
+++ Sources/First.cs
@@ -23,7 +23,7 @@ public class MainActivity extends Activity {
                 return true;
             // When the user clicks CANCEL ALARM, cancel the alarm. 
             case R.id.cancel_action:
-                alarm.cancelAlarmBranch(this);
+                alarm.cancelAlarmMaster(this);
                 return true;
         }
         return false;
